3f8c8310c7f35e502e097c0b5ff9a47dc60f1dac
ShoMasegi <m2.b0c27@gmail.com>
2018-10-15 01:07:19 +0900

    add list at chat screen

--- views/screens/MatchingScreen.js
+++ views/screens/MatchingScreen.js
@@ -5,7 +5,19 @@ import {
     Button,
     Platform,
     StyleSheet,
+    Text,
+    FlatList
 } from 'react-native'
+
+const _rows = [
+    {id: 0, text: 'View'},
+    {id: 1, text: 'Text'},
+    {id: 2, text: 'Image'},
+    {id: 3, text: 'ScrollView'},
+    {id: 4, text: 'ListView'},
+]
+
+const _extractKey = ({id}) => id
 
 export default class SettingsScreen extends React.Component {
 
@@ -17,14 +29,29 @@ export default class SettingsScreen extends React.Component {
         headerTintColor: '#fff'
     }
 
-    pushChatScreen = () => {
+    _onPressItem = () => {
         this.props.navigation.navigate('Chat')
+    }
+
+    _renderItem = ({item}) => {
+        return (
+            <TouchableOpacity onPress={this._onPressItem}>
+                <Text style={styles.row}>
+                    {item.text}
+                </Text>
+            </TouchableOpacity>
+        )
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Button title='Swipe Left' onPress={this.pushChatScreen}/>
+                <FlatList
+                    style={styles.flatList}
+                    data={_rows}
+                    renderItem={this._renderItem}
+                    keyExtractor={_extractKey}
+                />
             </View>
         )
     }
@@ -37,5 +64,13 @@ const styles = StyleSheet.create({
     },
     contentContainer: {
         paddingTop: 30,
+    },
+    flatList: {
+        marginTop: 20,
+    },
+    row: {
+        padding: 15,
+        marginBottom: 10,
+        backgroundColor: 'skyblue',
     },
 })
